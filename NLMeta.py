0ee522ccc501d812f84a9893479fbb30b331830b
Nathan - Lithia <nathanlol5@live.com.au>
2022-01-19 21:12:49 +1030

    Metadata generator

---
+++ NLMeta.py
@@ -0,0 +1,34 @@
+import os
+import io
+import hashlib
+
+
+# make a folder at ./mods/ called nlmeta
+if not os.path.exists("mods/nlmeta"):
+    os.makedirs("mods/nlmeta")
+
+
+mods = os.listdir("mods")
+metafiles = os.listdir("mods/nlmeta")
+md5 = hashlib.md5()
+BLOCK_SIZE = 65536
+
+
+for mod in mods:
+    if mod.endswith((".NLMeta")):
+        os.remove("mods/"+mod)
+
+
+for file in metafiles:
+    if mod.endswith((".NLMeta")):
+        os.remove("mods/nlmeta/"+mod)
+
+
+for mod in mods:
+    if mod.endswith((".jar")):
+        modhash = hashlib.md5(open(f'mods/'+mod,'rb').read()).hexdigest()
+        f = open(f"mods/nlmeta/{mod}.NLMeta", "a")
+        f.write(f"{mod}\n{modhash}")
+        f.close()
+        print(f"{modhash} {mod}")
+
---
+++ hash.bat
@@ -0,0 +1,2 @@
+python NLMeta.py
+pause
